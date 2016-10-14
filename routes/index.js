2a24a69b2efca340eca8bc252fead29b568800ec
Marc Majcher <majcher@gmail.com>
2016-10-14 12:07:10 -0500

    initial checkin

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/booyah', (req, res) => {
+  res.send('booyah');
+});
+
+module.exports = router;
