238faaf257e23039e367745f7ed2f654879efc0c
Marc Majcher <marc.majcher@galvanize.com>
2016-10-14 16:01:09 -0500

    basic registration/authentication

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,6 @@
 'use strict';
 
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const express = require('express');
+const router = express.Router();
 
 module.exports = router;
