9a8a186415807c035f44bce9bcc052fc6ae22cbe
Marc Majcher <majcher@gmail.com>
2016-10-24 15:55:51 -0500

    fixed seed

--- routes/map.js
+++ routes/map.js
@@ -9,14 +9,8 @@ router.get('/', (req, res, next) => {
   var myData = {};
   knex('locations').then((cities) => {
     myData.locations = cities.reduce(function(l,c) {l[c.id]=c;delete l[c.id].id;return l},{});
-  //   anonymous {
-  // id: 1952,
-  // name: 'Gatesville',
-  // latitude: 31.4352,
-  // longitude: -97.7439 },
     knex('city_link').then((links) => {
       myData.connections = links;
-  // anonymous { city1: 1867, city2: 1956, distance: 53900 },
       res.send(myData);
     });
   });
