b0bc53cc224b5895a3fd51d7888602e8e36b3df2
Marc Majcher <majcher@gmail.com>
2016-10-28 10:10:32 -0500

    failing at livereload/browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 'use strict';
 
 var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
 var concat = require('gulp-concat');
 var del = require('del');
 var gulp = require('gulp');
@@ -13,7 +11,7 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 
-gulp.task('default', ['clean', 'build', 'jshint', 'watch', 'server']);
+gulp.task('default', ['clean', 'build', 'jshint', 'watch', 'nodemon']);
 gulp.task('build', ['sass']);
 
 gulp.task('clean', function() {
@@ -60,52 +58,21 @@ gulp.task('sass', function() {
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(autoprefixer())
-    .pipe(gulp.dest('app/static/css'))
-    .pipe(reload({
-      stream: true
-    }));
+    .pipe(gulp.dest('app/static/css'));
 });
 
 gulp.task('nodemon', function(callback) {
-  var called = false;
   return nodemon({
-      script: 'app/www',
-      watch: ['app/app.js']
-    })
-    .on('start', function() {
-      if (!called) {
-        callback();
-      }
-      called = true;
-    })
-    .on('restart', function onRestart() {
-      setTimeout(function reload() {
-        browserSync.reload({
-          stream: false
-        });
-      }, 500);
-    });
-});
-
-
-gulp.task('server', ['nodemon'], function() {
-  browserSync({
-    proxy: 'http://localhost:3000',
-    port: 4000,
-    browser: ['google-chrome']
+    script: 'app/www',
+    watch: ['app/app.js']
   });
 });
 
-gulp.task('sync', function() {
-  browserSync.reload();
-});
-
-// configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
-  gulp.watch('app/**/*.js', ['jshint', reload]);
+  gulp.watch('app/**/*.js', ['jshint']);
   gulp.watch('test/**/*.js', ['jshint']);
-  gulp.watch('app/views/**/*.ejs', ['sync']);
-  gulp.watch('src/scss/**/*.scss', ['sass', 'sync']);
+  gulp.watch('app/views/**/*.ejs');
+  gulp.watch('src/scss/**/*.scss', ['sass']);
 });
 
 // https://www.mikestreety.co.uk/blog/advanced-gulp-file
