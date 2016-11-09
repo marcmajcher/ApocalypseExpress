69f7566ca488767bfb51013befaff0e167692a75
Marc Majcher <majcher@gmail.com>
2016-11-09 16:45:15 -0600

    map edit test

--- test/admin.js
+++ test/admin.js
@@ -8,73 +8,56 @@ const util = require('./_util');
 
 let adminCookie;
 let userCookie;
-let req;
 
 describe('Admin', () => {
   before(util.rollback);
+  before((done) => {
+    request(app).post('/login')
+      .send(`email=${util.users.testUser.email}&password=${util.users.testUser.password}`)
+      .end((err, res) => {
+        userCookie = util.getCookie(res);
+        done();
+      });
+  });
+  before((done) => {
+    request(app).post('/login')
+      .send(`email=${util.users.adminUser.email}&password=${util.users.adminUser.password}`)
+      .end((err, res) => {
+        adminCookie = util.getCookie(res);
+        done();
+      });
+  });
 
   it('should prevent non-logged-in users from getting into /admin', (done) => {
     request(app)
       .get('/admin')
       .set('Accept', 'text/html')
-      .expect(302)
+      .expect(500, done);
+  });
+
+  it('should prevent non-admin users from getting into /admin', (done) => {
+    const req = request(app)
+      .get('/admin')
+      .set('Accept', 'text/html');
+    req.cookies = userCookie;
+    req.expect(500, done);
+  });
+
+  it('should allow admin users to hit the dashboard', (done) => {
+    const req = request(app)
+      .get('/admin')
+      .set('Accept', 'text/html');
+    req.cookies = adminCookie;
+    req.expect(200)
       .expect('Content-Type', /text/)
-      .end((err, res) => {
-        res.headers.location.should.equal('/');
+      .end((err2, res2) => {
+        res2.text.should.match(/ApoX Admin/);
         done();
       });
   });
 
-  it('should prevent non-admin users from getting into /admin', (done) => {
-    request(app)
-      .post('/login')
-      .set('Accept', 'text/html')
-      .send(`email=${util.users.testUser.email}&password=${util.users.testUser.password}`)
-      .expect(302)
-      .expect('Content-Type', /text/)
-      .end((err, res) => {
-        res.headers.location.should.equal('/game');
-        userCookie = util.getCookie(res);
-
-        req = request(app)
-          .get('/admin')
-          .set('Accept', 'text/html');
-        req.cookies = userCookie;
-        req
-          .expect(302)
-          .expect('Content-Type', /text/)
-          .end((err2, res2) => {
-            res2.headers.location.should.equal('/');
-            done();
-          });
-      });
-  });
-
-  it('should allow admin users to hit the dashboard', (done) => {
-    request(app)
-      .post('/login')
-      .set('Accept', 'text/html')
-      .send(`email=${util.users.adminUser.email}&password=${util.users.adminUser.password}`)
-      .expect(302)
-      .expect('Content-Type', /text/)
-      .end((err, res) => {
-        res.headers.location.should.equal('/game');
-        adminCookie = util.getCookie(res);
-        req = request(app)
-          .get('/admin')
-          .set('Accept', 'text/html');
-        req.cookies = adminCookie;
-        req.expect(200)
-          .expect('Content-Type', /text/)
-          .end((err2, res2) => {
-            res2.text.should.match(/ApoX Admin/);
-            done();
-          });
-      });
-  });
-
   it('should return full map data for a logged in admin', (done) => {
-    req = request(app)
+    const req = request(app)
       .get('/map')
       .set('Accept', 'text/json');
     req.cookies = adminCookie;
@@ -92,7 +75,7 @@ describe('Admin', () => {
   });
 
   it('should allow an admin to edit a map location', (done) => {
-    req = request(app)
+    const req = request(app)
       .patch('/admin/map/location/2')
       .set('Accept', 'text/json');
     req.cookies = adminCookie;
@@ -103,5 +86,14 @@ describe('Admin', () => {
         res.body.status.should.equal('ok');
         done();
       });
+  });
+
+  it('should not allow a non-admin user to edit a map location', (done) => {
+    const req = request(app)
+      .patch('/admin/map/locaion/2')
+      .set('Accept', 'text/json');
+    req.cookies = userCookie;
+    req.send(util.locations.editLocation)
+      .expect(500, done);
   });
 });
