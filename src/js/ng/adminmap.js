21a0901a91a27f51e769e336bfcfd0801c360146
Marc Majcher <majcher@gmail.com>
2016-11-15 02:18:21 -0600

    making progress on angularizing the admin map

--- src/js/ng/adminmap.js
+++ src/js/ng/adminmap.js
@@ -7,26 +7,217 @@
     .controller('AdminMapController', ['$http', function map($http) {
       const vm = this;
 
-      vm.closeDetails = function close() {
-        $('#detailPanel').removeClass('out');
+      vm.location = {};
+      vm.mapData = {};
+      vm.showDetailPanel = true;
+      vm.dataLoaded = false;
+
+      vm.closeDetailPanel = function close() {
+        vm.showDetailPanel = false;
       };
 
-      vm.updateDetails = function update() {
-        if ($('#detailPanel #locid').val() > 0) {
-          // TODO: add waiting spinner
-          $http.patch(`/admin/map/location/${$('#detailPanel #locid').val()}`, {
-            name: $('#detailPanel #name').val(),
-            longitude: $('#detailPanel #longitude').val(),
-            latitude: $('#detailPanel #latitude').val(),
-            description: $('#detailPanel #description').val(),
-            population: $('#detailPanel #population').val(),
-            tech: $('#detailPanel #tech').val(),
-            type: $('#detailPanel #type').val()
-          })
-          .then((/* res */) => {
-            // console.log('cool.');
+      vm.loadMapData = function load() {
+        $http.get('/map')
+          .then((res) => {
+            vm.mapData.locations = res.data.locations;
+            vm.mapData.connections = res.data.connections;
+            vm.dataLoaded = true;
           });
+      };
+
+      vm.updateLocationDetails = function update() {
+        const loc = vm.location;
+        if (loc.id > 0) {
+          // TODO: add waiting spinner
+          $http.patch(`/admin/map/location/${loc.id}`, {
+              name: loc.name,
+              longitude: loc.longitude,
+              latitude: loc.latitude,
+              description: loc.description,
+              population: loc.population,
+              tech: loc.tech,
+              type: loc.type
+            })
+            .then(() => {
+              // console.log('cool.'); res
+            });
         }
       };
+
+      vm.loadMapData();
     }]);
+
+  angular.module('apox').directive('adminmap', () => {
+    const baseDotSize = 3;
+    const baseConWidth = 3;
+    const xScale = 435.30;
+    const yScale = -506.5;
+    const xOffset = 43647;
+    const yOffset = 15855;
+
+    function locToPoint(loc) {
+      return new paper.Point((loc.longitude * xScale) + xOffset, (loc.latitude * yScale) + yOffset);
+    }
+
+    function pointToLatLong(point) {
+      return {
+        longitude: (point.x - xOffset) / xScale,
+        latitude: (point.y - yOffset) / yScale
+      };
+    }
+
+    /* Location dot mouse event handlers */
+
+    function rolloverLocation(event) {
+      event.target.children.locname.visible = true;
+    }
+
+    function rolloutLocation(event) {
+      event.target.children.locname.visible = false;
+    }
+
+    /* Navigation methods */
+
+    // function changeZoom(oldZoom, delta, c, p) {
+    //   const factor = 1.05;
+    //   let newZoom = oldZoom;
+    //   if (delta > 0) {
+    //     newZoom *= factor;
+    //   }
+    //   if (delta < 0) {
+    //     newZoom /= factor;
+    //   }
+    //   // return newZoom;
+    //   const beta = oldZoom / newZoom;
+    //   const pc = p.subtract(c);
+    //   const a = p.subtract(pc.multiply(beta)).subtract(c);
+    //   return {
+    //     newZoom,
+    //     a
+    //   };
+    // }
+    //
+    // function changeCenter(oldCenter, deltaX, deltaY, factor) {
+    //   const offset = new paper.Point(deltaX, deltaY).multiply(factor);
+    //   return oldCenter.subtract(offset);
+    // }
+
+    function mousedownLocation(event) {
+      event.target.children.dot.fillColor.alpha = 0.5;
+      event.target.scope.$apply(() => {
+        event.target.scope.admin.location = event.target.location;
+      });
+    }
+
+    function mouseupLocation(event) {
+      event.target.children.dot.fillColor.alpha = 1;
+      event.preventDefault();
+    }
+
+    function dragLocation(event) {
+      const target = event.target;
+      target.position += event.delta;
+      const location = pointToLatLong(target.position);
+      target.location.longitude = location.longitude;
+      target.location.latitude = location.latitude;
+      $('#detailPanel #locx').val(target.location.latitude);
+      $('#detailPanel #locy').val(target.location.longitude);
+      for (let i = 0; i < target.location.paths.length; i++) {
+        target.location.paths[i].x = target.children.dot.position.x;
+        target.location.paths[i].y = target.children.dot.position.y;
+      }
+      // updatePositionDetail(location);
+      event.stopPropagation();
+    }
+
+    return {
+      restrict: 'A',
+      controller: 'AdminMapController',
+      controllerAs: 'admin',
+      link: (scope, element) => {
+        paper.setup(element.get(0));
+        const mapLayer = new paper.Layer();
+        mapLayer.texasMap = new paper.Raster('/img/texasmap.jpg');
+
+        scope.$watch('admin.dataLoaded', () => {
+          const data = scope.admin.mapData;
+          if (data.locations && data.connections) {
+            /* calculate location points */
+            Object.keys(data.locations).forEach((id) => {
+              data.locations[id].point = locToPoint(data.locations[id]);
+              data.locations[id].paths = [];
+            });
+
+            /* Draw connections */
+            for (let i = 0; i < data.connections.length; i++) {
+              const connection = data.connections[i];
+              const loc1 = data.locations[connection.loc1];
+              const loc2 = data.locations[connection.loc2];
+
+              const path = new paper.Path.Line(loc1.point, loc2.point);
+              path.strokeColor = 'black';
+              path.strokeWidth = baseConWidth;
+
+              loc1.paths.push(path.segments[0].point);
+              loc2.paths.push(path.segments[1].point);
+            }
+
+            /* render locations */
+            const textOffset = new paper.Point(0, -15); // eslint-disable-line no-magic-numbers
+            Object.keys(data.locations).forEach((id) => {
+              const location = data.locations[id];
+              const dot = new paper.Path.Circle({
+                center: location.point,
+                radius: baseDotSize * Math.ceil(Math.log10(location.population)),
+                fillColor: 'black', // set color/halo according to faction
+                name: 'dot'
+              });
+
+              const text = new paper.PointText(location.point + textOffset);
+              text.justification = 'center';
+              text.fillColor = '#3333ff';
+              text.strokeColor = '#9999cc';
+              text.content = location.name;
+              text.name = 'locname';
+              text.visible = false;
+              text.fontSize = 24;
+
+              const locGroup = new paper.Group([dot, text]);
+              locGroup.location = location;
+              locGroup.scope = scope;
+              locGroup.onMouseEnter = rolloverLocation;
+              locGroup.onMouseLeave = rolloutLocation;
+              locGroup.onMouseDrag = dragLocation;
+              locGroup.onMouseDown = mousedownLocation;
+              locGroup.onMouseUp = mouseupLocation;
+            });
+          }
+        });
+
+        mapLayer.onMouseDrag = (event) => {
+          event.target.position += event.delta;
+          // paper.project._needsUpdate = true;
+          paper.project.view.update();
+        };
+        paper.view.center = new paper.Point(1100, 500); // eslint-disable-line no-magic-numbers
+      }
+    };
+  });
 })();
+
+// $('#mapCanvas').bind('mousewheel', (event) => {
+//   const dx = event.originalEvent.wheelDeltaX;
+//   const dy = event.originalEvent.wheelDeltaY;
+//
+//   if (event.altKey) {
+//     const mousePos = new Point(event.offsetX, event.offsetY);
+//     const z = changeZoom(view.zoom, dy, view.center, mousePos);
+//     view.zoom = z.newZoom;
+//     // view.center = view.center.add(z.a);
+//     event.preventDefault();
+//   }
+//   else {
+//     view.center = changeCenter(view.center, dx, dy, 1);
+//     event.preventDefault();
+//   }
+// });
