de88b88a23e45b60c9ef046ad2a4ab00f33d1c05
Marc Majcher <majcher@gmail.com>
2016-11-16 01:49:08 -0600

    :art: angularizing and adding stuff to game page

--- seeds/dev/010_admin.js
+++ seeds/dev/010_admin.js
@@ -8,13 +8,12 @@ exports.seed = knex => knex('users').del()
   .then(() => knex('drivers').insert({
     name: driverName,
     location: 1
-  }))
-  .then(() => knex('drivers').where('name', driverName).first()
-    .then(driver => knex('users').insert({
-      email: 'majcher@gmail.com',
-      firstname: 'Marc',
-      lastname: 'Majcher',
-      hashedPassword: '$2a$12$Qs2FsKTK1tsaXGO/wc0YVOTI/doAPALiFWacy/Uku4NOShVopeGQm',
-      role: 'admin',
-      driverid: driver.id
-    })));
+  }, '*'))
+  .then(drivers => knex('users').insert({
+    email: 'majcher@gmail.com',
+    firstname: 'Marc',
+    lastname: 'Majcher',
+    hashedPassword: '$2a$12$Qs2FsKTK1tsaXGO/wc0YVOTI/doAPALiFWacy/Uku4NOShVopeGQm',
+    role: 'admin',
+    driverid: drivers[0].id
+  }));
