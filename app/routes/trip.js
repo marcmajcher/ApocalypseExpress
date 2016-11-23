292c7751e276af7e2ba1ff8ae3a23a9dcd57b30f
Marc Majcher <majcher@gmail.com>
2016-11-23 01:31:42 -0600

    :white_check_mark: :sparkles: trip routes and tests

--- app/routes/trip.js
+++ app/routes/trip.js
@@ -8,7 +8,7 @@ const util = require('./_util');
 
 router.use(util.loginRequired);
 
-// | GET | */trip* | Get current trip information
+/* Return current trip info */
 router.get('/', (req, res) => {
   util.knex('trips').where('driverid', req.session.user.driverid).orderBy('sequence')
     .then((trips) => {
@@ -18,49 +18,79 @@ router.get('/', (req, res) => {
     });
 });
 
-// | POST | */trip* | Start trip
+/* Create a new trip with given destination or destinations */
+router.put('/', (req, res, next) => {
+  util.knex('trips').where('driverid', req.session.user.driverid).del()
+    .then(() => {
+      const ids = (Array.isArray(req.body.destination)) ?
+        req.body.destination : [req.body.destination];
+      const values = ids.map((element, index) => ({
+        driverid: req.session.user.driverid,
+        sequence: index + 1,
+        locationid: element
+      }));
+      util.knex('trips').insert(values)
+        .then(() => {
+          res.send('ok');
+        })
+        .catch((err) => {
+          const error = new Error(`Trip DB error: ${err}`);
+          error.status = 500;
+          next(error);
+        });
+    });
+});
+
+/* Append the given destination to the current trip */
+// TODO: create trip sequence index table?
+router.patch('/', (req, res, next) => {
+  util.knex('trips').where('driverid', req.session.user.driverid).max('sequence').first()
+    .then((max) => {
+      const seq = max.max + 1;
+      util.knex('trips').insert({
+          driverid: req.session.user.driverid,
+          sequence: seq,
+          locationid: req.body.destination
+        })
+        .then(() => {
+          res.send('ok');
+        })
+        .catch((err) => {
+          const error = new Error(`Trip DB error: ${err}`);
+          error.status = 500;
+          next(error);
+        });
+    });
+});
+
+/* Begin current trip */
 router.post('/', (req, res) => {
   // TODO: use timer to travel
   // TODO: add 'traveling' column, check that not already traveling
   // TODO: check to verify that destination is adjacent to current location
   const destinationQuery = util.knex('trips').where('driverid', req.session.user.driverid)
     .orderBy('sequence').first()
-    .select('destination');
-
-  util.knex('drivers').where('driverid', req.session.user.driverid)
-    .update('location', destinationQuery)
-    .then(() => {
-      res.send('ok');
+    .select('locationid')
+    .then((destination) => {
+      util.knex('drivers').where('id', req.session.user.driverid)
+      .update('location', destination.locationid)
+      .then(() => {
+        res.send('ok');
+      });
     });
 });
 
-// | PUT | */trip* | Create new trip with destination id[s] {id/[ids]}
-router.put('/', (req, res) => {
-  util.knex('trips').where('driverid', req.session.user.driverid).del()
-    .then(() => {
-      const ids = (Array.isArray(req.body.destination)) ?
-        req.body.destination : [req.body.destination];
-      util.knex.insert(ids.map((element, index) => ({
-        driverid: req.session.user.driverid,
-        sequence: index,
-        locationid: element
-      })));
-      res.send('ok');
-    });
-});
-
-// | PATCH | */trip* | Add destination id[s] to current trip route {id/[ids]}
-router.patch('/', (req, res) => {
-  res.send('ok');
-});
-
-// | DELETE | */trip* | Clear current trip
-router.delete('/', (req, res) => {
+/* Clear current trip */
+router.delete('/', (req, res, next) => {
   util.knex('trips').where('driverid', req.session.user.driverid).del()
     .then(() => {
       res.send('ok');
+    })
+    .catch((err) => {
+      const error = new Error(`Trip DB error: ${err}`);
+      error.status = 500;
+      next(error);
     });
 });
-
 
 module.exports = router;
