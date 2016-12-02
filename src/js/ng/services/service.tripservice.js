86dabb022884ce8d23b0416ec6f7706c8db5c698
Marc Majcher <majcher@gmail.com>
2016-12-02 01:08:57 -0500

    delete route button

--- src/js/ng/services/service.tripservice.js
+++ src/js/ng/services/service.tripservice.js
@@ -21,24 +21,32 @@
       setNextDestination: function setNextDestination(id) {
         return $q((resolve, reject) => {
           $http.put(tripRoute, {
-            destination: id
-          })
-          .then((res) => {
-              resolve(res.data);
-            },
-            (err) => {
-              reject(err);
-            });
+              destination: id
+            })
+            .then((res) => {
+                resolve(res.data);
+              },
+              (err) => {
+                reject(err);
+              });
         });
       },
       clearTrip: function clearTrip() {
-
+        return $q((resolve, reject) => {
+          $http.delete(tripRoute)
+            .then((res) => {
+                resolve("ok");
+              },
+              (err) => {
+                reject(err);
+              });
+        });
       },
       beginTrip: function beginTrip() {
 
       },
       addDestination: function addDestination(id) {
-        
+
       }
     };
   };
