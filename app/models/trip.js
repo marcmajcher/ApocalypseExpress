3f77f5de41069dc8fe89e00032500ff2faa7e3b5
Marc Majcher <majcher@gmail.com>
2017-01-05 02:39:25 -0600

    working on trip model

--- app/models/trip.js
+++ app/models/trip.js
@@ -3,10 +3,34 @@
 /* eslint-env node */
 
 const util = require('../_util');
+const Location = require('./location');
 
 exports.get = driverid => util.knex('trips').where('driverid', driverid)
   .join('locations', 'trips.locationid', 'locations.id')
   .select('trips.locationid', 'locations.name', 'trips.sequence')
   .orderBy('sequence');
 
-exports.del = driverid => util.knex('trips').where('driverid', driverid).del();
+const deleteTrip = driverid => util.knex('trips').where('driverid', driverid).del();
+exports.del = deleteTrip;
+
+/* Create a new trip for the given driver to the given location, returns location */
+exports.create = (driverid, destinationid) =>
+  util.knex('connections').where({
+    end: destinationid,
+    start: util.knex('drivers').where('id', driverid).select('location')
+  })
+  .then((connections) => {
+    if (connections.length > 0) {
+      return deleteTrip(driverid).then(() =>
+        util.knex('trips').insert({
+          driverid,
+          sequence: 1,
+          locationid: destinationid,
+          distance: connections[0].distance
+        })
+        .returning('locationid')
+        .then(ids => Location.get(ids[0]))
+      );
+    }
+    return undefined;
+  });
--- app/routes/trip.js
+++ app/routes/trip.js
@@ -9,30 +9,6 @@ const Trip = require('../models/trip');
 
 router.use(util.loginRequired);
 
-/* DB functions */
-
-function adjacentConnections(driverid, destinationid, next) {
-  return util.knex('connections').where({
-      end: destinationid,
-      start: util.knex('drivers').where('id', driverid).select('location')
-    })
-    .then((connections) => {
-      if (connections.length === 0) {
-        const error = new Error(`Destination ${destinationid} not adjacent`);
-        error.status = 500;
-        next(error);
-      }
-      return connections;
-    })
-    .catch((err) => {
-      const error = new Error(`Trip DB error: ${err}`);
-      error.status = 500;
-      next(error);
-      return 0;
-    });
-}
-
-/* Trip Routes*/
 
 /* Return current trip info */
 router.get('/', (req, res, next) => {
@@ -60,41 +36,24 @@ router.get('/', (req, res, next) => {
 
 router.put('/', /* isNotTraveling, */ (req, res, next) => {
   const driverid = req.session.user.driverid;
-  adjacentConnections(driverid, req.body.destination, next)
-    .then((connections) => {
-      if (connections.length > 0) {
-        Trip.del(driverid)
-          .then(() => {
-            const trip = {
-              driverid: req.session.user.driverid,
-              sequence: 1,
-              locationid: req.body.destination,
-              distance: connections[0].distance
-            };
-            util.knex('trips').insert(trip)
-              .returning('locationid')
-              .then((ids) => {
-                util.knex('locations').where('id', ids[0]).first()
-                  .then((location) => {
-                    res.send({
-                      ok: true,
-                      id: location.id,
-                      name: location.name
-                    });
-                  });
-              })
-              .catch((err) => {
-                const error = new Error(`Trip DB error: ${err}`);
-                error.status = 500;
-                next(error);
-              });
-          });
-      }
+
+  Trip.create(driverid, req.body.destination)
+    .then((location) => {
+      res.send({
+        ok: true,
+        id: location.id,
+        name: location.name
+      });
+    })
+    .catch((err) => {
+      const error = new Error(`Trip DB error: ${err}`);
+      error.status = 500;
+      next(error);
     });
 });
 
 /* Begin current trip */
-router.post('/', /* isNotTraveling, */ (req, res /* , next */) => { // TODO: catch errors for delete
+router.post('/', /* isNotTraveling, */ (req, res/* , next */) => { // TODO: catch errors for delete
   const driverid = req.session.user.driverid;
   // TODO: use timer to travel (instant for admin)
   // TODO: add 'traveling' column, check that not already traveling
