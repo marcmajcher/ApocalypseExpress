247dfa1cd4b45bebb90bcb6a33eb128269515136
Marc Majcher <majcher@gmail.com>
2017-01-05 01:34:23 -0600

    moved map route location methods to location model

--- app/models/location.js
+++ app/models/location.js
@@ -4,6 +4,65 @@
 
 const util = require('../_util');
 
+/* Convert locations array into indexed object */
+function indexLocations(locations) {
+  return locations.reduce((last, cur) => {
+    last[cur.id] = cur;
+    return last;
+  }, {});
+}
+
+/* Select all locations from db and index */
+exports.getAllLocations = () => util.knex('locations')
+  .then(locations => ({
+    locations: indexLocations(locations)
+  }))
+  .catch(error => error);
+
+/* Select all connections from db */
+exports.getAllConnections = mapData =>
+  util.knex('connections')
+  .then((connections) => {
+    mapData.connections = connections;
+    return mapData;
+  })
+  .catch(error => error);
+
+/* Select locations from db for a given user */
+exports.getUserLocations = driverId =>
+  util.knex('locations')
+  .join('driver_visited', {
+    'locations.id': 'driver_visited.locationid',
+    'driver_visited.driverid': driverId
+  })
+  .select('id', 'name', 'latitude', 'longitude', 'population', 'tech', 'factionid', 'type')
+  .then(locations => ({
+    locations: indexLocations(locations)
+  }))
+  .catch(error => error);
+
+exports.getUserConnections = mapData =>
+  util.knex('connections')
+  .whereIn('start', Object.keys(mapData.locations))
+  .then((connections) => {
+    mapData.connections = connections;
+    return mapData;
+  })
+  .catch(error => error);
+
+exports.getConnectedLocations = mapData =>
+  util.knex('locations')
+  .whereIn('id', mapData.connections.map(el => el.end))
+  .then((locations) => {
+    locations.forEach((el) => {
+      mapData.locations[el.id] = el;
+    });
+    // mapData.locations = locations;
+    return mapData;
+  })
+  .catch(error => error);
+
+
 /* Get the info for a single location given an id */
 exports.get = id => util.knex('locations').where('id', id).first();
 
--- app/routes/map.js
+++ app/routes/map.js
@@ -5,79 +5,16 @@
 const express = require('express');
 const router = express.Router();
 const util = require('../_util');
+const Location = require('../models/location');
 
 router.use(util.loginRequired);
-
-/* Convert locations array into indexed object */
-function indexLocations(locations) {
-  return locations.reduce((last, cur) => {
-    last[cur.id] = cur;
-    return last;
-  }, {});
-}
-
-/* Select all locations from db and index */
-function getAllLocations() {
-  return util.knex('locations')
-    .then(locations => ({
-      locations: indexLocations(locations)
-    }))
-    .catch(error => error);
-}
-
-/* Select all connections from db */
-function getAllConnections(mapData) {
-  return util.knex('connections')
-    .then((connections) => {
-      mapData.connections = connections;
-      return mapData;
-    })
-    .catch(error => error);
-}
-
-/* Select locations from db for a given user */
-function getUserLocations(driverId) {
-  return util.knex('locations')
-    .join('driver_visited', {
-      'locations.id': 'driver_visited.locationid',
-      'driver_visited.driverid': driverId
-    })
-    .select('id', 'name', 'latitude', 'longitude', 'population', 'tech', 'factionid', 'type')
-    .then(locations => ({
-      locations: indexLocations(locations)
-    }))
-    .catch(error => error);
-}
-
-function getUserConnections(mapData) {
-  return util.knex('connections')
-    .whereIn('start', Object.keys(mapData.locations))
-    .then((connections) => {
-      mapData.connections = connections;
-      return mapData;
-    })
-    .catch(error => error);
-}
-
-function getConnectedLocations(mapData) {
-  return util.knex('locations')
-    .whereIn('id', mapData.connections.map(el => el.end))
-    .then((locations) => {
-      locations.forEach((el) => {
-        mapData.locations[el.id] = el;
-      });
-      // mapData.locations = locations;
-      return mapData;
-    })
-    .catch(error => error);
-}
 
 /* GET /map route */
 
 router.get('/', (req, res, next) => {
   if (req.session.user.role === 'admin') {
-    getAllLocations()
-      .then(getAllConnections)
+    Location.getAllLocations()
+      .then(Location.getAllConnections)
       .then((mapData) => {
         res.send(mapData);
       })
@@ -86,9 +23,9 @@ router.get('/', (req, res, next) => {
       });
   }
   else {
-    getUserLocations(req.session.user.driverid)
-      .then(getUserConnections)
-      .then(getConnectedLocations)
+    Location.getUserLocations(req.session.user.driverid)
+      .then(Location.getUserConnections)
+      .then(Location.getConnectedLocations)
       .then((mapData) => {
         res.send(mapData);
       })
