53ce0d73fbf8fd2cbe78f54ecebbd5a0e80f1c64
Marc Majcher <majcher@gmail.com>
2017-01-26 13:15:00 -0600

    ng socket service stub

---
+++ src/js/ng/services/socketservice.js
@@ -0,0 +1,13 @@
+(() => {
+  'use strict';
+
+  /* eslint-env jquery, browser */
+
+  const socketService = function socketService() {
+    return {
+
+    };
+  };
+
+  angular.module('apox').factory('SocketService', ['$http', socketService]);
+})();
