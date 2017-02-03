1f99e5f2958a921fd9f25d85015d79224ab46ab9
Marc Majcher <majcher@gmail.com>
2017-02-03 00:47:37 -0600

    trip progress bar

--- src/js/ng/game/trip.progress.component.js
+++ src/js/ng/game/trip.progress.component.js
@@ -8,9 +8,12 @@
       trip: '<'
     },
     template: `
-    <div class="trip-progress">
-    {{$ctrl.trip.origin}} --> {{$ctrl.trip.destination}} <br/>
-      {{$ctrl.trip.progress}} / {{$ctrl.trip.distance}}
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" style="width: {{100*($ctrl.trip.progress/$ctrl.trip.distance)}}%"></div>
+    </div>
+    <div>
+      <span class="pull-left">{{$ctrl.trip.origin}}</span>
+      <span class="pull-right">{{$ctrl.trip.destination}}</span>
     </div>
     `
   });
