3dddc246069472a6f01281519fa2d8f23c9edbab
Marc Majcher <majcher@gmail.com>
2017-03-19 05:53:21 -0500

    .

--- app/data/events.js
+++ app/data/events.js
@@ -2,3 +2,10 @@
 
 /* In here, we're going to build a grammar for events while traveling.
    We'll start off with color events, and go from there. */
+
+-
+weather
+  -
+  creatures -
+  weird things -
+  people(at a distance)
