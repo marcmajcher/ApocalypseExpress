cfd128ebfa7a9c2f0f630c72af7cba67ffc9b182
Marc Majcher <majcher@gmail.com>
2017-03-20 21:56:08 -0500

    events

--- app/data/events.js
+++ app/data/events.js
@@ -3,9 +3,64 @@
 /* In here, we're going to build a grammar for events while traveling.
    We'll start off with color events, and go from there. */
 
--
-weather
-  -
-  creatures -
-  weird things -
-  people(at a distance)
+/*
+Time-specific events
+  sunrise
+  sunset
+  noon
+  night
+
+Location-specific events
+
+Weather events
+  dry patch
+  sudden rainstorm
+  no clouds lightning
+  sandstorm/duststorm
+  dust devils
+
+Creature encounters
+  giant scorpions (or other insects)
+  antler bunnies
+  squished roadkill [animal]
+  flock of [ominous flying birds/animals]
+
+Weirdness
+  strange lights in the distance
+  voices from nowhere
+  smell of [something]
+  all your hairs stand on end
+  static fields
+  sudden metallic taste
+  sudden silence
+  a trembling in the earth
+
+Time modifiers
+  sudden
+  gradually
+  all at once
+
+People
+  other harmless travelers
+  other dangerous travelers that ignore you
+  small camps on the side of the road
+
+Distance modifiers
+  on the side of the road
+  in the distance
+  a few miles ahead
+  above you
+
+Death
+  bones bleaching in the sun
+  a corpse tied to a pole
+  a pile of what looks like hair
+  skin stretched across a road sign
+
+Signs of life
+  an abandoned stuffed bear
+  a pair of shoes in the road
+  a blown out tire
+
+
+*/
