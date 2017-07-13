f2abd62f4bbfb3cc17e316fb56d6405f0569016a
Marc Majcher <majcher@gmail.com>
2017-07-13 10:01:45 -0500

    cleanup

--- src/js/services/locationservice.js
+++ src/js/services/locationservice.js
@@ -37,29 +37,5 @@
 
   }
 
-  // const locationService = function locationService($http, $q) {
-  //   return {
-  //     getCurrentLocation() {
-  //       return $q((resolve, reject) => {
-  //         $http.get(locationRoute)
-  //           .then((location) => {
-  //               resolve(location.data);
-  //             },
-  //             (err) => {
-  //               reject(err);
-  //             });
-  //       });
-  //     },
-  //     getDistanceFromId(location, id) {
-  //       for (let i = 0; i < location.connections.length; i++) {
-  //         if (location.connections[i].id === id) {
-  //           return location.connections[i].distance;
-  //         }
-  //       }
-  //       return -1; // eslint-disable-line no-magic-numbers
-  //     }
-  //   };
-  // };
-
   angular.module('apox').service('LocationService', ['$http', '$q', LocationService]);
 })();
